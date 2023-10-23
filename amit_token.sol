31193cddc1bddabc511317574e958218b36c319a
AmitAgrawal123 <142304896+AmitAgrawal123@users.noreply.github.com>
2023-10-23 22:10:38 +0530

    Update amit_token.sol

--- amit_token.sol
+++ amit_token.sol
@@ -1,1 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CustomToken is ERC20, ERC20Burnable {
+    constructor(address initialOwner) ERC20("agrawal", "agw") {
+        _mint(initialOwner, 1000000 * 10 ** uint256(decimals()));
+    }
+}
